7e828ebfb1a55132025c025e07faa08e00166d05
Isaac-Sedcole <isaacsedcolei@gmail.com>
2021-02-04 15:06:25 +1300

    running low on power updating games

--- client/components/Games.jsx
+++ client/components/Games.jsx
@@ -1,9 +1,56 @@
 
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {GetSteamIdByUsername} from '../apis/steam'
 
 const Games = (props) => {
+
+  const [games, setGames] = useState(null)
+  const [showGames, setShowGames] = useState(false)
+  const [ids, setIds] = useState([
+    {
+      id: ""
+    }
+  ])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    GetSteamIdByUsername()
+      .then(fetchedId => {
+        setIds(currentId => {
+          console.log("these are the ids ",ids)
+          return [
+            ...currentId,
+            {id: fetchedId}
+          ]
+        })
+      })
+  }
+
+  const bigFunc = () => {
+    setShowGames(!showGames)
+    GetSteamIdByUsername()
+    .then(fetchedId => {
+      setIds(currentId => {
+        return [
+          ...currentId,
+          {id: fetchedId}
+        ]
+      })
+    })
+  }
+
+  const displayInfo = () => {
+    (<p>{ids.response.steamid}</p>)
+  }
+
+
     return (
       <>
+      <button onClick={bigFunc}>Show Games</button>
+      {showGames && displayInfo()}
       {/* <ul>
               <li>
                   <Link to `store.steampowered.com/app/{appid}/{gameName}/> - gameName is has space is _
